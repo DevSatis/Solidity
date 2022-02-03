8235c8c4ffb304bcce1c4a836154bcdc3cfbd3e9
"Satish Kumar" <"sksatishk9@gmail.com">
2022-02-03 22:55:10 +0530

    1st

---
+++ stateVariable.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >= 0.6.0 < 0.9.0;
+
+contract StateVariable{
+    uint public age = 10;
+
+    function setAge()public {
+        age = 25;
+    }
+}
